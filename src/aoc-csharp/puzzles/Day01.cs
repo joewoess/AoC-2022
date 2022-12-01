75ad1fc2fdfa52ded10614a87ab4d769b2497c95
joewoess <joewoess@gmail.com>
2022-12-01 07:19:09 +0100

    day 1 implementation

---
+++ src/aoc-csharp/puzzles/Day01.cs
@@ -0,0 +1,58 @@
+namespace aoc_csharp.puzzles;
+
+public class Day01 : IPuzzle
+{
+    private readonly string[] _input;
+
+    public Day01(string filename)
+    {
+        _input = File.ReadAllLines(filename);
+    }
+
+    public string? FirstPuzzle()
+    {
+        var sum = 0;
+        List<int> numbers = new List<int>();
+        foreach (var line in _input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                numbers.Add(sum);
+                sum = 0;
+            }
+            else
+            {
+                sum += int.Parse(line);
+            }
+        }
+        numbers.Add(sum);
+
+
+        var maxCalories = numbers.Max();
+        Printer.DebugMsg($"Elf with max calories had {maxCalories} total calories.");
+        return maxCalories.ToString();
+    }
+
+    public string? SecondPuzzle()
+    {
+        var sum = 0;
+        List<int> numbers = new List<int>();
+        foreach (var line in _input)
+        {
+            if (string.IsNullOrWhiteSpace(line))
+            {
+                numbers.Add(sum);
+                sum = 0;
+            }
+            else
+            {
+                sum += int.Parse(line);
+            }
+        }
+        numbers.Add(sum);
+
+        var maxCalories = numbers.OrderDescending().Take(3).Sum();
+        Printer.DebugMsg($"Top 3 elves had {maxCalories} total calories.");
+        return maxCalories.ToString();
+    }
+}
